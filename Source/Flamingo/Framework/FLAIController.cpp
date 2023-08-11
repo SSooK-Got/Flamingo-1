e6d8d3e4b0bd5d19ae40641d543008b9be7f7210
winkey83 <winkey83@gmail.com>
2023-08-11 15:22:48 +0900

    add AIController

---
+++ Source/Flamingo/Framework/FLAIController.cpp
@@ -0,0 +1,1 @@
+#include "FLAIController.h"
---
+++ Source/Flamingo/Framework/FLAIController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "FLAIController.generated.h"
+
+UCLASS()
+class FLAMINGO_API AFLAIController : public AAIController
+{
+	GENERATED_BODY()
+};
